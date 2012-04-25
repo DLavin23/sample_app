13c806546bd50f4323d4542092285507b02bfce5
DLavin23 <daniel.e.lavin@gmail.com>
2012-04-24 21:47:31 -0500

    Finished user edit, update, index, and destroy actions. 2 tests still not passing

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,13 @@
 FactoryGirl.define do
-  factory User do
-    name      "Michael Hartl"
-    email     "michael@example.com"
-    password  "foobar"
+  factory :user do
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password "foobar"
     password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
   end
 end
+
