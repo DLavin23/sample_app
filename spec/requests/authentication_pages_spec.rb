299729197138952c655044938e3a240fa88bb0fe
DLavin23 <daniel.e.lavin@gmail.com>
2012-05-16 15:53:48 -0500

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -40,7 +40,6 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
-      
       it { should_not have_link('Sign in', href: signin_path) }
 
       describe "followed by signout" do
@@ -71,6 +70,18 @@ describe "Authentication" do
         end
       end
       
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+        
       describe "in the Microposts controller" do
         describe "submitting to the create action" do
           before { post microposts_path }
@@ -93,14 +104,24 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
         
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
         
-        describe "submitting to the update action" do
-          before { put user_path(user) }
-          specify { response.should redirect_to(signin_path) }
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+        
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
         end
       end
     end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
-describe 'pages' do
-  
+describe "pages" do
+
   subject { page }
-  
-  describe 'Home page' do
+
+  describe "Home page" do
     before { visit root_path }
-    
-    it { should have_selector('h1', text: "Sample App") }
+
+    it { should have_selector('h1',    text: 'Sample App') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
-    
+
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -19,36 +19,44 @@ describe 'pages' do
         sign_in user
         visit root_path
       end
-      
+
       it "should render the user's feed" do
         user.feed.each do |item|
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 follower",  href: followers_user_path(user)) }
+      end
     end
   end
-  
+
   describe "Help page" do
     before { visit help_path }
-    
-    it { should have_selector('h1', text: "Help us Help you") }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Help' }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
-  
-  describe 'About page' do
+
+  describe "About page" do
     before { visit about_path }
-    
-    it { should have_selector('h1', text: 'About') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| About' }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
-  
-  describe 'Contact' do
+
+  describe "Contact page" do
     before { visit contact_path }
-    
-    it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', title: full_title('')) }
-    it { should_not have_selector 'title', text: '| Contact' }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
